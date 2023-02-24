dcb3c789ba9bea3191cea9c5d666c7d992ede552
znooker <znooker_18@hotmail.com>
2023-02-24 10:01:45 +0100

    uppdatering

--- Admin/indexADMIN.php
+++ Admin/indexADMIN.php
@@ -1,1 +1,8 @@
-hej svej
+<?php include('./Component/admin_header.php') ?>
+    <body>
+    
+    <h2>Här visas massa skit!</h2>
+       
+    </body>
+
+    <?php include('./Component/admin_footer.php') ?>
