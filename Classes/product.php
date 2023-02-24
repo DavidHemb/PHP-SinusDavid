259e463aea5811b729eaab3f1d8a47c775bfb5fd
DavidHemb <124883264+DavidHemb@users.noreply.github.com>
2023-02-24 10:36:15 +0100

    ADMIN select done

--- Classes/product.php
+++ Classes/product.php
@@ -3,8 +3,9 @@ require('config.php');
 class ProductClass
 {
     //ADMIN Function
-    static function filldatabase($pruduct_id, $title, $price, $color, $product_description, $imagepath, $stock, $date_created, $date_updated, $is_published)
+    static function addproduct($pruduct_id, $title, $price, $color, $product_description, $imagepath, $stock, $date_created, $date_updated, $is_published)
     {
+        //Varibles in database sent in by calling function
         // Create connection
         $conn = connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
         // Check connection
@@ -29,5 +30,33 @@ class ProductClass
             echo "Error: " . $query . "<br>" . $conn->error;
         }
     }
+    //ADMIN Function
+    static function selectproduct($title)
+    {
+        //Varible title in database sent in by calling function
+        // Create connection
+        $conn = connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
+        // Check connection
+        if ($conn->connect_error) 
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        //SQL
+        $query = $conn->prepare("SELECT * FROM producs WHERE title=?");
+        $query->bind_param('s', $title);
+        $query->execute();
+        $result = $query->get_result();
+        $r = $result->fetch_array(MYSQLI_ASSOC);
+        //Return selected product in array 
+        if ($conn->query($query) === TRUE) 
+        {
+            return $r;
+        }
+        else 
+        {
+            echo "Error: " . $query . "<br>" . $conn->error;
+        }
+    }
+
 }
 ?>
