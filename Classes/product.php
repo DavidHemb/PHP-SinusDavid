839fdafac97d6a0e0fb97f2dd1b2ba5eb9c94198
DavidHemb <124883264+DavidHemb@users.noreply.github.com>
2023-02-24 09:58:21 +0100

    ADMINFlldatabase

--- Classes/product.php
+++ Classes/product.php
@@ -1,3 +1,35 @@
-<?php ?>
+<?php 
+class ProductClass
+{
+    function filldatabase($pruduct_id, $title, $price, $color, $product_description, $imagepath, $stock, $date_created, $date_updated, $is_published)
+    {
+        // Create connection
+        $conn = connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
+        // Check connection
+        if ($conn->connect_error) 
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        //SQL
+        $sql = 
+       "INSERT INTO Products (product_id, title, price, color, product_description, imagepath, stock, date_created, date_updated, is_published)
+        VALUES ('10', '0')";
 
-<?php ?>
+        $query = $conn->prepare("INSERT INTO Products (product_id, title, price, color, product_description, imagepath, stock, date_created, date_updated, is_published)
+        VALUES ('?', '?', '?', '?', '?', '?', '?', '?', '?', '?')");
+        $query->bind_param('isdsssiddi', $pruduct_id, $title, $price, $color, $product_description, $imagepath, $stock, $date_created, $date_updated, $is_published, );
+        $query->execute();
+        $result = $query->get_result();
+        $r = $result->fetch_array(MYSQLI_ASSOC);
+
+        if ($conn->query($sql) === TRUE) 
+        {
+            echo "New record created successfully";
+        }
+        else 
+        {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+?>
--- sinuslib.php
+++ sinuslib.php
@@ -2,27 +2,5 @@
 require('config.php');
 class sinuslib
 {
-    function filldatabase()
-    {
-        // Create connection
-        $conn = connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
-        // Check connection
-        if ($conn->connect_error) 
-        {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-        $sql = 
-       "INSERT INTO product_id (ParkingSpotID, OccupationStatus, OccupationStatus, OccupationStatus, OccupationStatus, OccupationStatus)
-        VALUES ('10', '0')";
-
-        if ($conn->query($sql) === TRUE) 
-        {
-            echo "New record created successfully";
-        }
-        else 
-        {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    }
+    
 }
