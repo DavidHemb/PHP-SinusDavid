c875208ff484f729863ed5c4d5195600d53fd53e
Kimpan261 <124882104+Kimpan261@users.noreply.github.com>
2023-02-28 17:48:54 +0100

    upd
    basds

--- Component/header.php
+++ Component/header.php
@@ -1,4 +1,3 @@
-include()	
 
 <!DOCTYPE html>
 
@@ -12,7 +11,10 @@ include()
 			<button>Menu</button>
 			<div class="dropdown-content">
 				<?php
+				SearchBarMetod();				
 				include('header.css');
+				require_once('headerfunc.php');
+
 					// Generate menu items dynamically from array
 					$menuItems = array('home', 'about', 'contact');
 					$userChoice = isset($_GET['choice']) ? $_GET['choice'] : '';
@@ -47,51 +49,7 @@ include()
 
 <?php
 require('../config.php');
-// Create connection
-$conn = connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
-if(isset($_POST["submit"]))
-{
-    $str = $_POST["search"];
-    $sqlQuery = $conn->prepare("SELECT * FROM products WHERE product_description = ?");
-    $sqlQuery->bind_param("s", $str);
-    $sqlQuery->execute();
-    $result = $sqlQuery->get_result();
-
-    if($row = $result->fetch_assoc())
-    {
-        ?>
-        <br><br><br>
-        <table> 
-            <tr>
-                <th>Name</th>
-                <th>Description</th>
-            </tr>
-            <tr>
-                <td><?php var_dump($row)//echo $row['name']; ?></td>
-                <td><?php
-				if (isset($row['products_catagories'])) {
-    echo $row['products_catagories'];
-} else {
-    // handle the case when the key doesn't exist
-} 
-echo 'ProductID' . $row['product_id'].'<br>'; 
-echo 'Title' . $row['title'].'<br>';
-echo 'Product_description' . $row['price'].'<br>';
-?></td>
-
-            </tr>
-        </table>
-        <?php
-    }
-    else {
-        echo "Name does not exist";
-    }
-}
 
 
 ?>
