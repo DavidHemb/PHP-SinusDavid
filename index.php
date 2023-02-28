fedc0005fcdc35789eeefb388066a1b1e9ac12c2
DavidHemb <124883264+DavidHemb@users.noreply.github.com>
2023-02-28 10:49:35 +0100

    Header

--- index.php
+++ index.php
@@ -43,6 +43,7 @@ $products = Product::ADMINviewProducts();
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./CSS/main.css">
+        <link rel="stylesheet" href="./CSS/header.css">
         <title>SINUS SKATEOARDS</title>
     </head>
     <body>
