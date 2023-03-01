ac7e7429e22f3a4b7eef4140728e652814d98f8d
DavidHemb <124883264+DavidHemb@users.noreply.github.com>
2023-03-01 13:05:47 +0100

    Update shoppingcart.php

--- shoppingcart.php
+++ shoppingcart.php
@@ -7,15 +7,4 @@ $arr = array("product_id"=>$product_id, "quantity"=>$quantity);
 
 
 new Row($arr["product_id"], $arr["quantity"]);
-var_dump($arr);
-
-$test = array();
-
-for ($i = 0; $i < count($arr); $i++) 
-{ ?>
-    <tr>
-        <td><?= $test["product_id"]->get_product_id(); ?></td>
-        <td><?= $test["quantity"]->get_quantity(); ?></td>
-    </tr>
-<?php } ?>
-*/
+?>
