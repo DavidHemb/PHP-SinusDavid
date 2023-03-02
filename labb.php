9c6bda6af5dc38da22d0d9eb5a2d90fb4f6143e9
znooker <znooker_18@hotmail.com>
2023-03-02 17:18:08 +0100

    Update labb.php

--- labb.php
+++ labb.php
@@ -51,6 +51,43 @@ echo "<pre>";
 var_dump($foo);
 echo "</pre>"; 
 
+//Gamla produktkortet TA INTE BORT - ANDREAS
+/* <div class="productcard">
+<tr>
+    <td><img src="./<?= $products[$i]->get_imagepath(); ?>" alt=" <?= $products[$i]->get_title(); ?> " border=0 height=600 width=600></img></td>
+    <p class="title"><?= $products[$i]->get_title(); ?></p>
+    <p></p>
+    <td>Stock:</td>
+    <td><?= $products[$i]->get_stock(); ?></td>
+    <p></p>
+    <td>Price:</td>
+    <td><?= $currencyFormatter->formatCurrency($products[$i]->get_price(), "SEK") ; ?></td>
+    <p style="margin-bottom: -50px;"></p>
+    <form action="productcard.php" method="POST">
+    <input type="hidden" name="product_id" value="<?= $products[$i]->get_product_id(); ?>">
+        <p></p>
+        <input class="button" style="background-color: brown; margin-top: 40px;" type="submit" value="More info">
+    </form>
+    <?php if($products[$i]->get_stock() > 0) { ?>
+        <form action="shoppingcart.php" method="POST">
+        <input type="hidden" name="product_id" value="<?= $products[$i]->get_product_id(); ?>">
+            <p></p>
+            <input class="button" style="float: right; background-color: green;" type="submit" value="Buy now">
+        </form>
+    <?php } else {?>
+        <p class="button" style="float: right; background-color: green; cursor: default;">Out of stock!</p>
+    <?php } ?>
+    <form action="picturetab.php" method="POST" target="_blank">
+    <input type="hidden" name="product_id" value="<?= $products[$i]->get_product_id(); ?>">
+        <p></p>
+        <input class="button" style=" background-color: black;" type="submit" value="View picture in new tab">
+    </form>
+</tr>
+</div>  */
+
+
+
+
  ?>
 
  
