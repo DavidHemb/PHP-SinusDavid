f1cc6ef7506499ccfcc145d5313a3e23e2859fb4
znooker <znooker_18@hotmail.com>
2023-03-03 14:57:11 +0100

    delete category
    test

--- Admin/views/delete_category.php
+++ Admin/views/delete_category.php
@@ -12,7 +12,7 @@ $categories = Category::ViewCategory();
         <input type="hidden" name="action" value="delete_category">
         <label for="category">Category:</label>
         <select name="category_id" id="category">
-            <?php for ($i = 0; $i < count($categories); $i++) { ?>
+            <?php for ($i = 1; $i < count($categories); $i++) { ?>
                 <option value="<?= $categories[$i]->get_category_id(); ?>"><?= $categories[$i]->get_title(); ?></option>
             <?php } ?>
         </select>
